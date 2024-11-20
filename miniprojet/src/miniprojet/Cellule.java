86160eb1efea8d6b3cd40c0e44afa7b924feab4f
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-11-20 15:17:16 +0100

    mini prjet test

---
+++ miniprojet/src/miniprojet/Cellule.java
@@ -0,0 +1,46 @@
+package miniprojet;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Administrateur
+ */
+public class Cellule {
+    private boolean presenceBombe;
+    private boolean devoilee;
+    private int nbBombesAdjacentes;
+
+    public boolean isPresenceBombe() {
+        return presenceBombe;
+    }
+
+    public int getNbBombesAdjacentes() {
+        return nbBombesAdjacentes;
+    }
+    public void placerBombe() {
+        presenceBombe=true;
+    }
+    public void revelerCellule() {
+        devoilee=true;
+    }
+
+    public void setNbBombesAdjacentes(int nbBombesAdjacentes) {
+        this.nbBombesAdjacentes = nbBombesAdjacentes;
+    }
+
+    @Override
+    public String toString() {
+        String texte="";
+        if(devoilee=false) texte="?";
+        else {
+            if (presenceBombe=true) texte="?";
+            else ;
+        }
+        return texte ;
+    }
+    
+}
