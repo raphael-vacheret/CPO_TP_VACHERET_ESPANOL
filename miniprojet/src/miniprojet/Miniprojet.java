73da6a277720fe1470ef1395effd39ed9ddb0f49
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-11-20 14:55:32 +0100

    mini prjet test

--- miniprojet/src/miniprojet/Miniprojet.java
+++ miniprojet/src/miniprojet/Miniprojet.java
@@ -14,7 +14,7 @@ public class Miniprojet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        if damien==damdamdeo raphael="content;)"
+        
     }
     
 }
