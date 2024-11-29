fd35383be5c7b61e764a71c03f72323a5c450d99
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-11-29 15:28:11 +0100

    mini prjet test

--- miniprojet/src/miniprojet/GrilleDeJeu.java
+++ miniprojet/src/miniprojet/GrilleDeJeu.java
@@ -29,8 +29,52 @@ public class GrilleDeJeu {
     
     public void placerBombesAleatoirement(){
         Random random= new Random();
-        int i= random.nextInt(10);
-        int j= random.nextInt(10);
+        for (int i=0;i<nbBombes;i++) {   
+            int h= random.nextInt(nbColonnes);
+            int l= random.nextInt(nbLignes);
+            if (matrice[h][l].isPresenceBombe()==true) {
+                i--;
+            }
+            else {
+                matrice[h][l].placerBombe();
+            }
+        }
+    }
+    public void calculerBombesAdjacentes() {
+        int BombesAdjacentes=0;
+        for (int i=0;i<nbColonnes;i++) {
+            for (int j=0;j<nbLignes;j++) {
+                if(matrice[i][j].isPresenceBombe()==false) {
+                    for (int h=i-1;h<i+1;h++) {
+                        for (int l=j-1;l<j+1;l++) {
+                            if (h>0) {
+                                if (l>0) {
+                                    if (matrice[i][j].isPresenceBombe()==true) {
+                                        BombesAdjacentes++;
+                                    }
+                                }
+                            }
+                        }
+                    }    
+                }
+            matrice[i][j].setNbBombesAdjacentes(BombesAdjacentes);
+            }
+        }
         
+    }
+    public void revelerCellule(int ligne, int colonne) {
+        matrice[ligne][colonne].revelerCellule();
+        if(matrice[ligne][colonne].isPresenceBombe()==true) {
+            //fin de la partie
+        }
+        else {
+            if (matrice[ligne][colonne].getNbBombesAdjacentes()==0) {
+                for (int h=ligne-1;h<ligne+1;h++) {
+                    for (int l=colonne-1;l<colonne+1;l++) {
+                        matrice[ligne][colonne].revelerCellule();                           
+                    }
+                }                    
+            }
+        }
     }
 }
---
+++ miniprojet/src/miniprojet/partie.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miniprojet;
+import miniprojet.GrilleDeJeu;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class partie {
+    GrilleDeJeu grille;
+    public void initialiserPartie() {
+        
+    }
+    public void tourDeJeu(int ligne, int colonne) {
+        
+    }
+    public void verifierVictoire() {
+        
+    }
+    public void demarrerPartie() {
+        
+    }
+}
