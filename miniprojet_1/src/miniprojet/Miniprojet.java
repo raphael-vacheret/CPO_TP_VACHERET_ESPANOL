1a72f55f56b479adfca69091544ebb7ba7674bda
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-12-02 16:43:32 +0100

    mini prjet avancement

---
+++ miniprojet_1/src/miniprojet/Miniprojet.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this templa */
+package miniprojet;
+
+public class Miniprojet {
+
+    public static void main(String[] args) {
+        // Création et démarrage de la partie
+        Partie partie = new Partie();
+        partie.demarrerPartie();
+    }
+}
+
