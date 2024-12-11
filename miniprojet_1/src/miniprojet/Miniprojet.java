3f2bf7809f3b0536c04c94bf25114309949c076e
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-12-11 15:27:45 +0100

    mini prjet avancement

--- miniprojet_1/src/miniprojet/Miniprojet.java
+++ miniprojet_1/src/miniprojet/Miniprojet.java
@@ -12,3 +12,7 @@ public class Miniprojet {
     }
 }
 
+// rajouté des vie                                                              3
+// rajouté un nouveau mode de difficulté/nouvelle grille                        2
+// crée des incertitudes                                                        4
+// crée des references (x/y) pour savoir ou il plus facilement ou l'on clique   1
